f57a5d064fc055beaf60ef3ce8f62c534e7261f6
“alex” <hoangvanduc2504@gmail.com>
2026-05-02 19:52:48 +0700

    sua file configuration 2

--- src/configurations/configuration.ts
+++ src/configurations/configuration.ts
@@ -1,5 +1,5 @@
 export const OAuthConfig = {
     clientId: "630022625960-5kps372gh04r47auqpbp0oepgo952uk8.apps.googleusercontent.com",
-    redirectUri: import.meta.env.VITE_URL + "/login",
+    redirectUri: import.meta.env.VITE_URL_REDIRECT,
     authUri: "https://accounts.google.com/o/oauth2/auth",
 };
