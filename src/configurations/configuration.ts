d24f4872c0404cf5db80113cec46eca96465c710
“alex” <hoangvanduc2504@gmail.com>
2026-05-02 18:08:27 +0700

    fix file configuration

--- src/configurations/configuration.ts
+++ src/configurations/configuration.ts
@@ -1,5 +1,5 @@
 export const OAuthConfig = {
     clientId: "630022625960-5kps372gh04r47auqpbp0oepgo952uk8.apps.googleusercontent.com",
-    redirectUri: "http://localhost:5173/login",
+    redirectUri: import.meta.env.VITE_URL + "/login",
     authUri: "https://accounts.google.com/o/oauth2/auth",
 };
