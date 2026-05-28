7e9e67985ce274dfc8ea34fe81c0970a3cbf8cd8
“alex” <hoangvanduc2504@gmail.com>
2026-05-28 09:14:05 +0700

    xoa import thua

--- src/layouts/user/favorites/Favorites.tsx
+++ src/layouts/user/favorites/Favorites.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import type { FavoriteResponse } from "../../../models/response/favorite/FavoriteResponse";
 import {
